2ae38db90b4185dcefe6a3c4c434e338b374ebc3
cjspd-oly <CJSPD.OLY+github@GMAIL.COM>
2025-02-22 06:11:21 +0530

    Create run_streamlit.sh

---
+++ dev_notes/run_streamlit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd ~/Documents/GitHub/GCP-SkillTracker
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Navigate to dev_notes
+cd dev_notes
+
+# Run Streamlit on port 8080
+python -m streamlit run private.py --server.port 8599
